102d027c6a16a7740bf3c993f9069d6a830913a0
Chukka-Abhinay <chukkaabhinay123@gmaiol.com>
2025-06-12 09:50:21 +0530

    scroller working but not for mouse

--- frontend/src/hero/Hero.jsx
+++ frontend/src/hero/Hero.jsx
@@ -8,18 +8,21 @@ const videoItems = [
   { id: 0, video: basketBallVid, name: 'basketBall' },
   { id: 1, video: footBallVid,   name: 'footBall' },
   { id: 2, video: tennisVid,     name: 'tennis' },
+  { id: 3, video: tennisVid,     name: 'tennis1' },
+  { id: 4, video: tennisVid,     name: 'tennis2' },
+  { id: 5, video: tennisVid,     name: 'tennis3' },
+  { id: 6, video: tennisVid,     name: 'tennis4' },
+  { id: 7, video: tennisVid,     name: 'tennis5' },
+  { id: 8, video: tennisVid,     name: 'tennis6' },
+  { id: 9, video: tennisVid,     name: 'tennis7' },
+  { id: 10, video: tennisVid,     name: 'tennis8' },
+  { id: 11, video: tennisVid,     name: 'tennis9' },
+  { id: 12, video: tennisVid,     name: 'tennis10' },
+  { id: 13, video: tennisVid,     name: 'tennis11' }
 ]
 
 const Hero = () => {
   const [action, setAction] = useState(0)
-  const scrollerRef = useRef(null)
-
-  useEffect(() => {
-    const selected = scrollerRef.current?.children[action]
-    if (selected) {
-      selected.scrollIntoView({ behavior: 'smooth', inline: 'center' })
-    }
-  }, [action])
 
   return (
     <div className='hero-container'>
@@ -28,8 +31,7 @@ const Hero = () => {
         src={videoItems[action].video}
         autoPlay loop muted playsInline
       />
-      <div className="selector-overlay" />
-      <div className="scroller" ref={scrollerRef}>
+      <div className="scroller" >
         {videoItems.map(item => (
           <div
             key={item.id}
