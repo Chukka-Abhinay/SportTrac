151f9c6fe2fe4ed33090f070778f2968383d3787
Adimula Ravi Shankar <ravishankaradimula2004@gmail.com>
2025-07-16 22:00:59 +0530

    before merging with ravi

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,8 @@ import Main from "../components/PlayerDetails/Main.jsx";
 import Schedule from "../components/Schedule/Schedule.jsx";
 import Leaderboard from "../components/Leaderboard/Leaderboard.jsx";
 import { useSport } from "../Context/SportContext";
-
+import { useSelector } from "react-redux";
+import AdminMenu from "./Admin/AdminMenu.jsx";
 const HomePage = () => {
   const { selectedSport } = useSport();
 
@@ -44,7 +45,7 @@ const HomePage = () => {
       <div ref={LeaderboardRef}>
         <Leaderboard />
       </div>
-      {userInfo.isAdmin && <AdminMenu />}
+      {userInfo?.isAdmin && <AdminMenu />}
     </>
   );
 };
