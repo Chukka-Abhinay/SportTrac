543e05e340a0c560931320916b88b11e915d843d
Chukka-Abhinay <chukkaahinay123@gmail.com>
2025-08-05 23:56:56 +0530

    connection

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 // ✅ For production, change `origin: "*"` to your Vercel frontend URL
-app.use(cors({ origin: "*", credentials: true })); 
+app.use(cors({ origin: "https://sport-trac-oxx3vyp69-chukka-abhinays-projects.vercel.app", credentials: true })); 
 
 app.use((req, res, next) => {
   req.io = app.get("io");
