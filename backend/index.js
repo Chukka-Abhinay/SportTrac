ad585be9cf7c9e8076a0ecb51c2ceb935193e445
Chukka-Abhinay <chukkaahinay123@gmail.com>
2025-08-06 22:50:46 +0530

    st1

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://sport-trac-pxm25v2k5-chukka-abhinays-projects.vercel.app",
+    origin: "https://sport-trac-rhf4gq1jo-chukka-abhinays-projects.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
